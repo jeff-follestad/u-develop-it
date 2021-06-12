a06a26558772eededd6657cf201d7eb361294612
Jeff Follestad <jeffrey.follestad@gmail.com>
2021-06-12 10:53:00 -0700

    added inputChecktests

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -7,13 +8,30 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // Your MySQL username,
+    user: 'root',
+    // Your MySQL password
+    password: 'Oakland2020!!',
+    database: 'election'
+  },
+  console.log('Connected to the election database.')
+)
+
 app.get('/', (req, res) => {
   res.json({
     message: 'Hello World'
   });
 });
 
-// Default response for any other request (Not Found)
+//db.query(`SELECT * FROM candidates`, (err, rows) => {
+  //console.log(rows);
+//});
+
+// Default "catch-all" response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
 });
