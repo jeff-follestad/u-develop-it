027768e4ba8f6db9ea0018911bae3751f058f36a
Jeff Follestad <jeffrey.follestad@gmail.com>
2021-06-12 13:14:27 -0700

    wrapped API endpoints in express.js routes

--- server.js
+++ server.js
@@ -21,31 +21,60 @@ const db = mysql.createConnection(
   console.log('Connected to the election database.')
 )
 
-app.get('/', (req, res) => {
-  res.json({
-    message: 'Hello World'
+// Get all candidates
+app.get('/api/candidates', (req, res) => {
+  const sql = `SELECT * FROM candidates`;
+
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: rows
+    });
   });
 });
 
-//db.query(`SELECT * FROM candidates`, (err, rows) => {
-  //console.log(rows);
-//});
+// Get a single candidate
+app.get('/api/candidate/:id', (req, res) => {
+  const sql = `SELECT * FROM candidates WHERE id = ?`;
+  const params = [req.params.id];
 
-// GET a single candidate
-//db.query(`SELECT * FROM candidates WHERE id = 5`, (err, row) => {
-  //if (err) {
-    //console.log(err);
-  //}
-  //console.log(row);
-//});
+  db.query(sql, params, (err, row) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: row
+    });
+  });
+});
 
 // Delete a candidate
-//db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
-  //if (err) {
-    //console.log(err);
-  //}
-  //console.log(result);
-//});
+app.delete('/api/candidate/:id', (req, res) => {
+  const sql = `DELETE FROM candidates WHERE id = ?`;
+  const params = [req.params.id];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.statusMessage(400).json({ error: res.message });
+    } else if (!result.affectedRows) {
+      res.json({
+        message: 'Candidate not found'
+      });
+    } else {
+      res.json({
+        message: 'deleted',
+        changes: result.affectedRows,
+        id: req.params.id
+      });
+    }
+  });
+});
 
 // Create a candidate
 const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
